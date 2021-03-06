29e80a528649c4f7ade0df1090095d9f385b6a37
Mayur Somani <mayur491@gmail.com>
2021-03-06 16:47:25 +0530

    Initial empty spring boot project.

---
+++ src/test/java/com/codemayur/pms/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemayur.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
